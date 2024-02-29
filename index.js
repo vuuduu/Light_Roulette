bfe240237f28f7a5f221145487ea0b02d2d1894f
Vu Nguyen <74219658+vuuduu@users.noreply.github.com>
2024-02-29 11:01:31 -0500

    add in start btn and score display

--- index.js
+++ index.js
@@ -1,52 +1,76 @@
-const LIGHT_DELAY = 150;
+const GAME_LIGHT_DELAY = 300;
+const USER_LIGHT_DELAY = 150;
+const DELAY_BUFFER = 50;
 const SCORE_DISPLAY = 'scoreDisplay';
+const GAME_BOXES = document.querySelectorAll(".game-box");
+
 let gameSeq = [];
 let userSeq = [];
-let userTurn = false;
 
-// Addign listening event to all of the game box
-const gameBoxes = document.querySelectorAll(".game-box");
-gameBoxes.forEach((gameBox) => {
+let userTurn = false;
+let gameOver = false;
+
+// Adding listening event to all of the game box
+GAME_BOXES.forEach((gameBox) => {
     gameBox.addEventListener("click", () => {
         if(userTurn) {
-            litUp(gameBox);
+            litUp(gameBox, USER_LIGHT_DELAY);
             
             setTimeout(() => {
                 console.log(gameBox.id);
-            }, 200);
+            }, USER_LIGHT_DELAY + DELAY_BUFFER);
         }
     });
 })
 
+function startGame() {
+    // Change the start button to score display
+    const bottomDisplay = document.getElementById("bottom-display");
+    bottomDisplay.innerHTML = updateBottomDisplay(SCORE_DISPLAY);
 
-function litUp(gameBox) {
+    /* begin the lighting sequence
+    while (!gameOver) {
+        let lightDelay = 0;
+
+        light a seq
+            if there's color in gameSeq
+                light the color 1 by 1
+                increase lightDelay depending on the len of gameSeq
+            
+            Light random box then add color to the gameSeq
+        
+        turn userTurn to true when all the gameSeq light up.
+    }
+    */
+
+}
+
+function lightASequence() {
+    // 
+    if (gameSeq.length !== 0) {
+
+    }
+}
+
+function litUp(gameBox, lightDelay) {
     gameBox.classList.add("lit-up");
     setTimeout(() => {
         gameBox.classList.remove("lit-up");
-    }, LIGHT_DELAY);
+    }, lightDelay);
 }
 
-function startGame() {
-    userTurn = true;
-
-    // Change the start button to score display
-    const bottomDisplay = document.getElementById("bottom-display");
-    bottomDisplay.innerHTML = updateBottomDisplay("scoreDisplay");
-
-}
-
-function updateBottomDisplay(style) {
-    if (style === SCORE_DISPLAY) {
-        style = `
-            <h3>Current Score: <div id="score" style="display: inline;">0</div></h3>
-        `;
+function updateBottomDisplay(htmlDisplay='') {
+    if (htmlDisplay === SCORE_DISPLAY) {
+        htmlDisplay = `
+            <h3>Current Score: 
+                <div id="score"">0</div>
+            </h3>`;
     }else {
-        style = `
+        htmlDisplay = `
             <button type="button" class="btn btn-primary" id="start-btn" onclick="startGame()">
                 Start Game
-            </button>
-        `;
+            </button>`;
     }
 
-    return style;
+    return htmlDisplay;
 }
